abd6ca978212d451fc824392bd95bc7bae14d7df
marcomastrorocco <marcomastroroccobackup@gmail.com>
2026-07-30 19:01:08 +1000

    fix: bust stylesheet cache for approved design

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 document.querySelectorAll('.nav-links a').forEach((link)=>link.addEventListener('click',()=>{const toggle=document.querySelector('.nav-toggle');if(toggle)toggle.checked=false;}));
+document.head.insertAdjacentHTML('beforeend','<link rel="stylesheet" href="/styles.css?v=06ea6d9">');
