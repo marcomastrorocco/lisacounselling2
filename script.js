67d11ab4c2f4c74f4de409c1fab807e2dbe80a87
marcomastrorocco <marcomastroroccobackup@gmail.com>
2026-07-31 10:08:54 +1000

    feat: add header swish logo and favicon

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 document.querySelectorAll('.nav-links a').forEach((link)=>link.addEventListener('click',()=>{const toggle=document.querySelector('.nav-toggle');if(toggle)toggle.checked=false;}));
-document.head.insertAdjacentHTML('beforeend','<link rel="stylesheet" href="/styles.css?v=logo-swish">');
+if(!document.querySelector('link[rel="icon"]'))document.head.insertAdjacentHTML('beforeend','<link rel="icon" href="/favicon.svg" type="image/svg+xml">');
+document.head.insertAdjacentHTML('beforeend','<link rel="stylesheet" href="/styles.css?v=header-swish">');
